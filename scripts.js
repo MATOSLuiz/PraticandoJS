bcdd03823f0c66ff0302888a249b2170f587cbe8
Luiz <luis231103@gmail.com>
2022-01-15 19:39:26 -0300

    Revisando conceitos importantes

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Object
+
+const person = {
+    name: 'Luiz',
+    age: 18,
+    weight: 61,
+    isHarryPotterFan: true
+}
+
+// console.log(`${person.name} is a ${person.isHarryPotterFan} Harry Potter fan!`)
+
+
+// Array
+
+const names = [
+    'Luiz',
+    'Roberto',
+    'Lucas'
+]
+
+//accessing values inside an array
+// console.log(names[2])
+
