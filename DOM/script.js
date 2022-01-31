216f95ac13aae2c0fd88fa325365927d35dcc11b
Luiz <luis231103@gmail.com>
2022-01-31 19:42:42 -0300

    Seleção de elementos pela DOM

---
+++ DOM/script.js
@@ -0,0 +1,22 @@
+//getElementbyId(element)
+//getElementsByClassName(HTMLCollection)
+//getElementsByTagName(HTMLCollection)
+
+const element = document.getElementsByTagName('h1')
+
+console.log(element)
+
+//Seletores
+
+//querySelector(element)
+//querySelectorAll(NodeList)
+
+const selector = document.querySelectorAll('paragrafo')
+
+console.log(selector)
+
+//Qual utilizar?
+
+// Depende da sua situação
+
+
