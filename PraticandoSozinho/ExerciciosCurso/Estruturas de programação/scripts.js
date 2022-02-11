6c217475ea84c1110222c2147b833780e37c068c
Luiz <luis231103@gmail.com>
2022-02-11 11:28:52 -0300

    Exercicio6 CursoJS

---
+++ PraticandoSozinho/ExerciciosCurso/Estruturas de programação/scripts.js
@@ -0,0 +1,15 @@
+// Ex1 Variaveis
+
+let idade = 19;
+let nome = "Luiz";
+let vivo = true;
+
+console.log(typeof vivo);
+console.log(typeof nome);
+console.log(typeof idade);
+
+
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
--- PraticandoSozinho/ExerciciosCurso/Tipos de Dados/scripts.js
+++ PraticandoSozinho/ExerciciosCurso/Tipos de Dados/scripts.js
@@ -1,45 +1,43 @@
 //Ex 1 Strings
 
-let firstName = "Luiz Henrique"
-let lastName = " Soares de Matos"
+let firstName = "Luiz Henrique";
+let lastName = " Soares de Matos";
 
-
-console.log("Meu primeiro nome é :", firstName)
-console.log(`Meu sobrenome é: ${lastName}`)
-console.log("\n")
-console.log("\n")
-console.log("\n")
-
+console.log("Meu primeiro nome é :", firstName);
+console.log(`Meu sobrenome é: ${lastName}`);
+console.log("\n");
+console.log("\n");
+console.log("\n");
 //Ex 2 Numbers
 
-console.log(1)
-console.log(2.25)
-console.log(1 + 1)
-console.log("\n")
-console.log("\n")
-console.log("\n")
+console.log(1);
+console.log(2.25);
+console.log(1 + 1);
+console.log("\n");
+console.log("\n");
+console.log("\n");
 
 //Ex 3 Booleans
 
-console.log(2 > 1)
-console.log(1 >= 524)
-console.log(1 != 1)
-console.log("\n")
-console.log("\n")
-console.log("\n")
+console.log(2 > 1);
+console.log(1 >= 524);
+console.log(1 != 1);
+console.log("\n");
+console.log("\n");
+console.log("\n");
 
 //Ex 4 And Or e Not
 
-console.log("Luiz" == "Luiz" && "Ana" == "Ana")
-console.log("Ana Júlia" == "Ana Júlia" || "Ana Júlia" == "Ana Julia")
-console.log(!("Ana Julia" == "Ana Julia"))
-console.log("\n")
-console.log("\n")
-console.log("\n")
+console.log("Luiz" == "Luiz" && "Ana" == "Ana");
+console.log("Ana Júlia" == "Ana Júlia" || "Ana Júlia" == "Ana Julia");
+console.log(!("Ana Julia" == "Ana Julia"));
+console.log("\n");
+console.log("\n");
+console.log("\n");
 
 //Ex 5 NaN
 
-console.log("oi" * "5")
+console.log("oi" * "5");
 
 //The END!
 
