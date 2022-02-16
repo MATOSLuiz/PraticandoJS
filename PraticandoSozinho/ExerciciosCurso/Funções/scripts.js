26cd3f6e17c1d9a701f269b33e39fc8c07f88e42
Luiz <luis231103@gmail.com>
2022-02-15 22:22:24 -0300

    Exercicio 17

--- PraticandoSozinho/ExerciciosCurso/Funções/scripts.js
+++ PraticandoSozinho/ExerciciosCurso/Funções/scripts.js
@@ -14,39 +14,3 @@ function mostraIdade(idade) {
 
 mostraIdade(15);
 
-// EX 18 soma de dois numeros
-
-function soma(a, b) {
-    return a + b;
-}
-
-console.log(soma(20,25));
-
-// //EX 19 numero aleatorio
-
-function numeroAleatorio(num) {
-    return Math.floor(Math.random() * num) + 1;
-}
-
-console.log(numeroAleatorio(50));
-
-console.log(numeroAleatorio(100));
-
-console.log(numeroAleatorio(500));
-
-//ex 20 autoescola
-
-function autoEscola(idade) {
-    if(idade < 18) {
-        console.log("Você não pode entrar na autoescola ainda!");
-    } else {
-        console.log("Você ja pode entrar na autoescola!");
-    }
-}
-
-autoEscola(18);
-
-autoEscola(16);
-
-autoEscola(22);
-
