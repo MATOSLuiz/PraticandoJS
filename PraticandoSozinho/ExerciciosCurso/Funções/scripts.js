7c44b3dcde573c06e3e078db95e495b4d82fa0bf
Luiz <luis231103@gmail.com>
2022-02-15 22:23:04 -0300

    Exercicio 19

--- PraticandoSozinho/ExerciciosCurso/Funções/scripts.js
+++ PraticandoSozinho/ExerciciosCurso/Funções/scripts.js
@@ -22,3 +22,23 @@ function soma(a, b) {
 
 console.log(soma(20,25));
 
+// EX 18 soma de dois numeros
+
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(20,25));
+
+// //EX 19 numero aleatorio
+
+function numeroAleatorio(num) {
+    return Math.floor(Math.random() * num) + 1;
+}
+
+console.log(numeroAleatorio(50));
+
+console.log(numeroAleatorio(100));
+
+console.log(numeroAleatorio(500));
+
