513457d2f723e149cc11c30cd0406cfaf23e4bbe
Luiz <luis231103@gmail.com>
2022-02-25 19:34:53 -0300

    Exercicio27(ArrayseObjetos)

---
+++ PraticandoSozinho/ExerciciosCurso/Arrays e Objetos/scripts.js
@@ -0,0 +1,13 @@
+//Exercicio 27
+
+let amigos = ["Gabriel Viccari", "Darlan", "Victor", "Mesaque", "Lucas"];
+
+console.log(amigos[0]);
+console.log(amigos[2]);
+console.log(amigos[3]);
+
+console.log("\n");
+console.log("\n");
+
+
+
